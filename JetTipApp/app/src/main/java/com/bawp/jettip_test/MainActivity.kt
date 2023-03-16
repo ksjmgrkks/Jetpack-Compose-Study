2663d3f52cbf3b869e542c2682e75b0d5ec4502b
김규성 <ksjmgrkks@gmail.com>
2023-03-16 13:52:34 +0900

    JetTipApp inputField 초기화

--- JetTipApp/app/src/main/java/com/bawp/jettip_test/MainActivity.kt
+++ JetTipApp/app/src/main/java/com/bawp/jettip_test/MainActivity.kt
@@ -3,18 +3,21 @@ package com.bawp.jettip_test
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.bawp.jettip_test.ui.theme.JetTipAppTheme
@@ -25,7 +28,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyApp {
-                Text("안녕 친구들")
+                TopHeader()
             }
 
         }
@@ -41,25 +44,44 @@ fun MyApp(content: @Composable () -> Unit) {
     }
 }
 
-@Preview
+//@Preview
 @Composable
-fun TopHeader() {
+fun TopHeader(totalPerPerson: Double = 134.0) {
     Surface(
         modifier = Modifier
             .fillMaxWidth()
             .height(150.dp)
-            .clip(shape = RoundedCornerShape(corner = CornerSize(12.dp)))
+            .clip(shape = RoundedCornerShape(corner = CornerSize(12.dp))),
+        color = Color(0xFFE9D7F7)
     ) {
-        Column() {
-
+        Column(
+            modifier = Modifier.padding(12.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            val total = "%.2f".format(totalPerPerson)
+            Text(text = "인당 지불 금액", style = MaterialTheme.typography.h5)
+            Text(text = "${total}원", style = MaterialTheme.typography.h4, fontWeight = FontWeight.ExtraBold)
         }
+
+    }
+
+}
+@Preview
+@Composable
+fun MainContent(){
+    Surface(
+        modifier = Modifier.padding(2.dp).fillMaxWidth(),
+        shape = RoundedCornerShape(corner = CornerSize(8.dp)),
+        border = BorderStroke(width = 1.dp, color = Color.LightGray)
+    ) {
 
     }
 
 }
 
 
-@Preview(showBackground = true)
+//@Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     MyApp {
--- JetTipApp/app/src/main/java/com/bawp/jettip_test/components/Components.kt
+++ JetTipApp/app/src/main/java/com/bawp/jettip_test/components/Components.kt
@@ -20,30 +20,6 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
-fun InputField(
-    modifier: Modifier = Modifier,
-    valueState: MutableState<String>,
-    labelId: String,
-    enabled: Boolean,
-    isSingleLine: Boolean = true,
-    keyboardType: KeyboardType = KeyboardType.Number,
-    imeAction: ImeAction = ImeAction.Next,
-    onAction: KeyboardActions = KeyboardActions.Default
-              ) {
-    OutlinedTextField(
-        value = valueState.value,
-        onValueChange = { valueState.value = it },
-        label = { Text(text =  labelId) },
-        leadingIcon = { Icon(imageVector = Icons.Rounded.AttachMoney,
-            contentDescription = "Money Icon")
-        },
-        singleLine = isSingleLine,
-        textStyle = TextStyle(fontSize = 18.sp, color = MaterialTheme.colors.onBackground),
-        modifier = modifier
-            .padding(bottom = 10.dp, start = 10.dp, end = 10.dp)
-            .fillMaxWidth(),
-        enabled = enabled,
-        keyboardOptions = KeyboardOptions(keyboardType = keyboardType, imeAction = imeAction),
-        keyboardActions = onAction
-                     )
+fun InputField(){
+
 }

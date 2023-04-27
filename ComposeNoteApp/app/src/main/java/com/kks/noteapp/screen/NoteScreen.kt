4553e4aa30d864b728b77cfa78a5cd8f0296925f
김규성 <ksjmgrkks@gmail.com>
2023-04-27 17:36:39 +0900

    compose note app creating a TextField

---
+++ ComposeNoteApp/app/src/main/java/com/kks/noteapp/screen/NoteScreen.kt
@@ -0,0 +1,46 @@
+package com.kks.noteapp.screen
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Notifications
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.kks.noteapp.R
+import com.kks.noteapp.components.NoteInputText
+
+@Composable
+fun NoteScreen(){
+    Column(modifier = Modifier.padding(6.dp)) {
+        TopAppBar(title = {
+                          Text(text = stringResource(id = R.string.app_name))
+        },
+            actions = {
+                Icon(imageVector = Icons.Rounded.Notifications, contentDescription = "Icon")
+            },
+        backgroundColor = Color(0xFFB3D1E9))
+
+        //Content
+        Column(modifier = Modifier.fillMaxWidth(),
+               horizontalAlignment = Alignment.CenterHorizontally) {
+            NoteInputText(text = "text", label = "label", onTextChange = {})
+        }
+        
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun NotesScreenPreview(){
+    NoteScreen()
+}

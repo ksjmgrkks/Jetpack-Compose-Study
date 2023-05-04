d6e57f8dd7a9426d8ae07960e9af0b306d572c09
김규성 <ksjmgrkks@gmail.com>
2023-05-04 19:42:13 +0900

    api 유료 이슈로 인해 WeatherForecast app -> Reader app 변경

---
+++ Reader App/app/src/main/java/com/kks/readerapp/ReaderApplication.kt
@@ -0,0 +1,7 @@
+package com.kks.readerapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ReaderApplication: Application()
--- WeatherForecast App/app/src/main/java/com/kks/weatherforecastapp/WeatherApplication.kt
+++
@@ -1,7 +0,0 @@
-package com.kks.weatherforecastapp
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class WeatherApplication: Application()

cc0a880db407aeacedf726922538205d786d4d5e
김규성 <ksjmgrkks@gmail.com>
2023-05-05 17:32:11 +0900

    [test] Reader App FireStore Test

--- Reader App/app/src/main/java/com/kks/readerapp/MainActivity.kt
+++ Reader App/app/src/main/java/com/kks/readerapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.kks.readerapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
@@ -12,6 +13,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.google.firebase.firestore.FirebaseFirestore
 import com.kks.readerapp.ui.theme.ReaderAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -30,8 +32,22 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun ReaderApp() {
     ReaderAppTheme {
+        val db = FirebaseFirestore.getInstance()
+        val user: MutableMap<String, Any> = HashMap()
+        user["firstName"] = "Harry"
+        user["lastName"] = "Kim"
+
         Surface(color = MaterialTheme.colors.background,
                 modifier = Modifier.fillMaxSize()) {
+                db.collection("users")
+                    .add(user)
+                    .addOnSuccessListener {
+                        Log.d("[kks]", "FireStore Success: ${it.id}")
+                    }.addOnFailureListener {
+                        Log.d("[kks]", "FireStore Failure: ${it.message}")
+                    }
+
+
                Column(verticalArrangement = Arrangement.Center,
                      horizontalAlignment = Alignment.CenterHorizontally) {
                     Text(text = "빌드 성공!")

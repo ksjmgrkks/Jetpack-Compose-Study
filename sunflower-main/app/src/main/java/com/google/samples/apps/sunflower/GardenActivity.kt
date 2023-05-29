81184b5d6088f64da3ca7a0fc3fcd7150bd93b87
김규성 <ksjmgrkks@naver.com>
2023-05-30 01:06:45 +0900

    sunflower app upload - 하나하나 주석 달면서 학습 예정

---
+++ sunflower-main/app/src/main/java/com/google/samples/apps/sunflower/GardenActivity.kt
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.samples.apps.sunflower
+
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.MenuProvider
+import androidx.core.view.WindowCompat
+import com.google.accompanist.themeadapter.material.MdcTheme
+import com.google.samples.apps.sunflower.compose.SunflowerApp
+import com.google.samples.apps.sunflower.compose.home.SunflowerPage
+import com.google.samples.apps.sunflower.viewmodels.PlantListViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+// TODO: update the superclass to ComponentActivity https://github.com/android/sunflower/issues/829
+@AndroidEntryPoint
+class GardenActivity : AppCompatActivity() {
+
+    private val viewModel: PlantListViewModel by viewModels()
+
+    private val menuProvider = object : MenuProvider {
+        override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+            menuInflater.inflate(R.menu.menu_plant_list, menu)
+        }
+
+        override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+            return when (menuItem.itemId) {
+                R.id.filter_zone -> {
+                    viewModel.updateData()
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Displaying edge-to-edge
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        setContent {
+            MdcTheme {
+                SunflowerApp(
+                    onAttached = { toolbar ->
+                        setSupportActionBar(toolbar)
+                    },
+                    onPageChange = { page ->
+                        when (page) {
+                            SunflowerPage.MY_GARDEN -> removeMenuProvider(menuProvider)
+                            SunflowerPage.PLANT_LIST -> addMenuProvider(
+                                menuProvider,
+                                this
+                            )
+                        }
+                    },
+                    plantListViewModel = viewModel,
+                )
+            }
+        }
+    }
+}
